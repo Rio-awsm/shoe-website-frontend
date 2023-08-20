d51e685d4d0995f40c787dca87611cbd49aecf4f
Supriyo Saha <mm.saha1982@gmail.com>
2023-08-20 14:43:16 +0530

    crating section structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
+import React from 'react';
+
+import Nav from './components/Nav';
+
+import {
+   CustomerReviews,
+   Footer,
+   Hero,
+   PopularProducts,
+   Services,
+   SpecialOffer,
+   Subscribe,
+   SuperQuality,
+ } from "./sections";
+ 
 
 const App = () => {
   return (
     <main className='relative'>
-      Nav
+      <Nav />
       <section className='xl:padding-1 wide:padding-r padding-b'>
-        Hero
+        <Hero />
       </section>
       <section className='padding'>
-         Popular product
+         <PopularProducts />
       </section>
       <section className='padding'>
-         Super quality
+         <SuperQuality />
       </section>
       <section className='padding-x py-10'>
-         Services
+         <Services />
       </section>
       <section className='padding'>
-         Special offer
+         <SpecialOffer />
       </section>
       <section className='bg-pale-blue padding'>
-         Reviews
+         <CustomerReviews />
       </section>
       <section className='padding-x sm:py-32 py-16 w-full'>
-         Subscribe
+         <Subscribe />
       </section>
       <section className='bg-black padding-x padding-t pb-8'>
-         Footer
+         <Footer />
       </section>
     </main>
   )
---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SpecialOffer = () => {
+  return (
+    <div>
+      Special offer
+    </div>
+  )
+}
+
+export default SpecialOffer
